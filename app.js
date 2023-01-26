ee323536aee78cbab152586e360ee911a999c9d8
Hexan Raa <hexanraa@gmail.com>
2023-01-26 18:41:43 +0500

    fixed book cards and created new book form

--- app.js
+++ app.js
@@ -1,54 +1,75 @@
-let myLibrary = [
-  {
-    title: "Harry Potter",
-    description:
-      "The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry",
-    author: "J. K. Rowling",
-    pages: 234,
-    publisheddate: "June 26, 1997",
-    read: true,
-  },
-  {
-    title: "A Game of Thrones",
-    description:
-      "A Game of Thrones is the first novel in A Song of Ice and Fire, a series of fantasy novels by American author George R. R. Martin",
-    author: "George R. R. Martin",
-    pages: 678,
-    publisheddate: "August 1, 1996",
-    read: false,
-  },
-  {
-    title: "Harry Potter",
-    description:
-      "The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry",
-    author: "J. K. Rowling",
-    pages: 234,
-    publisheddate: "June 26, 1997",
-    read: true,
-  },
-  {
-    title: "A Game of Thrones",
-    description:
-      "A Game of Thrones is the first novel in A Song of Ice and Fire, a series of fantasy novels by American author George R. R. Martin",
-    author: "George R. R. Martin",
-    pages: 678,
-    publisheddate: "August 1, 1996",
-    read: false,
-  },
-];
+let myLibrary = [];
 
 // Book Constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, date, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.date = date;
   this.read = read;
 }
 
 function addBookToLibrary() {
-  let newBook = new Book("abcd", "dsfasdg", "123", true);
-  let newBook2 = new Book("abcd", "dsfasdg", "123", true);
-  return myLibrary.push(newBook, newBook2);
+  myLibrary.push(
+    new Book(
+      "A Game of Thrones",
+      "George R. R. Martin",
+      "789",
+      "August 1, 1996",
+      true
+    )
+  );
+  myLibrary.push(
+    new Book("Harry Potter", "J. K. Rowling", "689", "June 26, 1997", false)
+  );
+  myLibrary.push(
+    new Book(
+      "A Game of Thrones",
+      "George R. R. Martin",
+      "789",
+      "August 1, 1996",
+      true
+    )
+  );
+  myLibrary.push(
+    new Book("Harry Potter", "J. K. Rowling", "689", "June 26, 1997", false)
+  );
+  myLibrary.push(
+    new Book(
+      "A Game of Thrones",
+      "George R. R. Martin",
+      "789",
+      "August 1, 1996",
+      true
+    )
+  );
+  myLibrary.push(
+    new Book("Harry Potter", "J. K. Rowling", "689", "June 26, 1997", false)
+  );
+  myLibrary.push(
+    new Book(
+      "A Game of Thrones",
+      "George R. R. Martin",
+      "789",
+      "August 1, 1996",
+      true
+    )
+  );
+  myLibrary.push(
+    new Book("Harry Potter", "J. K. Rowling", "689", "June 26, 1997", false)
+  );
+  myLibrary.push(
+    new Book(
+      "A Game of Thrones",
+      "George R. R. Martin",
+      "789",
+      "August 1, 1996",
+      true
+    )
+  );
+  myLibrary.push(
+    new Book("Harry Potter", "J. K. Rowling", "689", "June 26, 1997", false)
+  );
 }
 
 // function to Display Books
@@ -56,29 +77,22 @@ function displayBooks() {
   const booksContainer = document.querySelector(".books-container");
   myLibrary.forEach(book => {
     const card = document.createElement("div");
+    const bookName = document.createElement("h3");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookPublishedDate = document.createElement("p");
+    const bookRead = document.createElement("p");
+
     card.classList.add("card");
 
-    const bookName = document.createElement("h3");
     bookName.textContent = book.title;
-
-    const bookDescribe = document.createElement("p");
-    bookDescribe.textContent = book.description;
-
-    const bookAuthor = document.createElement("p");
     bookAuthor.textContent = "Author: " + book.author;
-
-    const bookPages = document.createElement("p");
     bookPages.textContent = "Pages: " + book.pages;
-
-    const bookPublishedDate = document.createElement("p");
-    bookPublishedDate.textContent = "Published Date: " + book.publisheddate;
-
-    const bookRead = document.createElement("p");
+    bookPublishedDate.textContent = "Published Date: " + book.date;
     bookRead.textContent = book.read;
 
     card.appendChild(bookName);
     card.appendChild(document.createElement("hr"));
-    card.appendChild(bookDescribe);
     card.appendChild(bookAuthor);
     card.appendChild(bookPages);
     card.appendChild(bookPublishedDate);
@@ -87,13 +101,14 @@ function displayBooks() {
   });
 }
 
+addBookToLibrary();
 displayBooks();
 
-console.log(myLibrary);
+const form = document.querySelector(".form-modal");
+const overlay = document.getElementById("overlay");
+const newBookBtn = document.querySelector(".newbookbtn");
 
-// const form = document.querySelector("div");
-// const newBookBtn = document.querySelector(".newbookbtn");
-
-// newBookBtn.addEventListener("click", () => {
-//   form.classList.toggle("hidden");
-// });
+newBookBtn.addEventListener("click", () => {
+  overlay.classList.add("active");
+  form.classList.add("active");
+});
